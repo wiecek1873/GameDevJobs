53c742d6e54e3f3d2a3e95d91574ad9a71f527aa
Michał Więcek <wiecek1873@gmail.com>
2023-06-22 16:01:50 +0200

    Removed ForeignKey property

--- GameDevJobs/GameDevJobs.Application/Dto/Offers/OfferDto.cs
+++ GameDevJobs/GameDevJobs.Application/Dto/Offers/OfferDto.cs
@@ -12,19 +12,14 @@ public class OfferDto
     public int Id { get; set; }
     public string Title { get; set; } = null!;
     public int CompanyId { get; set; }
-    [ForeignKey("CompanyId")]
     public virtual Company Company { get; set; } = null!;
     public int LocationId { get; set; }
-    [ForeignKey("LocationId")]
     public virtual Location Location { get; set; } = null!;
     public virtual int CategoryId { get; set; }
-    [ForeignKey("CategoryId")]
     public Category Category { get; set; } = null!;
-    [ForeignKey("WorkingTimeId")]
     public int WorkingTimeId { get; set; }
     public virtual WorkingTime WorkingTime { get; set; } = null!;
     public int? SeniorityId { get; set; }
-    [ForeignKey("SeniorityId")]
     public virtual Seniority? Seniority { get; set; }
     public int? SalaryMin { get; set; }
     public int? SalaryMax { get; set; }
