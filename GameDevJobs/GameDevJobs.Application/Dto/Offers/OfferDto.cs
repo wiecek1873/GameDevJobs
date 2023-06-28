028b976bd9fdf3d90a436ce78c938b66defe9733
Michał Więcek <wiecek1873@gmail.com>
2023-06-28 16:21:26 +0200

    Added migration. Removed references to objects from DTO

--- GameDevJobs/GameDevJobs.Application/Dto/Offers/OfferDto.cs
+++ GameDevJobs/GameDevJobs.Application/Dto/Offers/OfferDto.cs
@@ -1,26 +1,14 @@
-﻿using GameDevJobs.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GameDevJobs.Application.Dto.Offers;
 
-namespace GameDevJobs.Application.Dto.Offers;
 public class OfferDto
 {
     public int Id { get; set; }
     public string Title { get; set; } = null!;
     public int CompanyId { get; set; }
-    public virtual Company Company { get; set; } = null!;
     public int LocationId { get; set; }
-    public virtual Location Location { get; set; } = null!;
     public virtual int CategoryId { get; set; }
-    public Category Category { get; set; } = null!;
     public int WorkingTimeId { get; set; }
-    public virtual WorkingTime WorkingTime { get; set; } = null!;
     public int? SeniorityId { get; set; }
-    public virtual Seniority? Seniority { get; set; }
     public int? SalaryMin { get; set; }
     public int? SalaryMax { get; set; }
     public DateOnly Date { get; set; }
