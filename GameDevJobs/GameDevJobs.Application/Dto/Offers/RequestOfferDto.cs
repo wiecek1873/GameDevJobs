24a9613bcf9d5d0a5685af8829b3700b37b3e32b
Michał Więcek <wiecek1873@gmail.com>
2023-06-28 18:27:58 +0200

    Added validation to request offer dto

--- GameDevJobs/GameDevJobs.Application/Dto/Offers/RequestOfferDto.cs
+++ GameDevJobs/GameDevJobs.Application/Dto/Offers/RequestOfferDto.cs
@@ -1,16 +1,37 @@
-﻿namespace GameDevJobs.Application.Dto.Offers;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameDevJobs.Application.Dto.Offers;
 
 public class RequestOfferDto
 {
+    [Required(ErrorMessage = "Title name is required")]
     public string Title { get; set; } = null!;
-    public string Company { get; set; } = null!;
-    public string Location { get; set; } = null!;
-    public string Category { get; set; } = null!;
-    public string WorkingTimeId { get; set; } = null!;
-    public string? Seniority { get; set; }
+
+    [Required(ErrorMessage = "Company name is required")]
+    public string CompanyName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Location name is required")]
+    public string LocationName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Category name is required")]
+    public string CategoryName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Working time name is required")]
+    public string WorkingTimeName { get; set; } = null!;
+
+    public string? SeniorityName { get; set; }
+
     public int? SalaryMin { get; set; }
+
     public int? SalaryMax { get; set; }
+
+    //todo Figure out how to validate this
+    [Required(ErrorMessage = "Date is required")]
     public DateOnly Date { get; set; }
+
+    [Required(ErrorMessage = "Views count is required")]
     public int Views { get; set; }
+
+    [Url]
     public string Url { get; set; } = null!;
 }
